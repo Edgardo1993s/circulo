1cd8f5abb1946e402731dce770a9fd942e52066c
selvin <selvinedgardo.com>
2021-05-31 21:33:21 -0600

    Area del Circulo

---
+++ area_circulo.py
@@ -0,0 +1,4 @@
+print("programa para calcular el area de un ciruclo")
+radio=input("Ingrese el radio del circulo: ")
+area=float(radio)*float(radio)*3.1416
+print("El area del circulo es: "+str(area))
